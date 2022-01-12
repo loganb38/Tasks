18f4771034948b0af51411e8be91e6651d17cfa2
loganb38 <lcb0023@mix.wvu.edu>
2022-01-12 08:25:41 -0500

    commented on install

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,7 +1,7 @@
 ## Access token: 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 
 ## Don't delete above line
 ### blah  
-install.packages("swirl")
+#install.packages("swirl")
 library(swirl)
 swirl()
 Logan
