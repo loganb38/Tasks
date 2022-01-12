c1bdc4a41cb955ef6ae95464b02d2d15ce3917fd
loganb38 <lcb0023@mix.wvu.edu>
2022-01-12 12:38:07 -0500

    logan bower Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,15 +1,31 @@
-## Access token: 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
+## Access token:  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
 ## Don't delete above line
 ### blah  
 #install.packages("swirl")
 library(swirl)
 swirl()
 Logan
-
-
-
-
+library(swirl)
+swirl()
+Logan
 1
-
-
-
+5+7
+x <- 5+7
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 1000
+my_sqrt <- sqrt(z - 1)
+1
+my_sqrt
+my_div <- z / my_sqrt
+2
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
+1
